7bdfb25444e2c418a8416d10351a608326fded23
Leo <88931222+Leo4Bey@users.noreply.github.com>
2024-05-04 00:32:09 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+from colorama import Fore, Back, Style
+
+
+print(Fore.RED + 'Kırmızı metin' + Fore.RESET)
+print(Fore.BLACK + 'Siyah metin' + Fore.RESET)
+print(Fore.GREEN + 'Yeşil metin' + Fore.RESET)
+print(Fore.YELLOW + 'Sarı metin' + Fore.RESET)
+print(Fore.BLUE + 'Mavi metin' + Fore.RESET)
+print(Fore.MAGENTA + 'Magenta metin' + Fore.RESET)
+print(Fore.CYAN + 'Camgöbeği metin' + Fore.RESET)
+print(Fore.WHITE + 'Beyaz metin' + Fore.RESET)
+
+print(Back.GREEN + 'Yeşil arka plan' + Back.RESET)
+print(Back.BLACK + 'Siyah arka plan' + Back.RESET)
+print(Back.RED + 'Kırmızı arka plan' + Back.RESET)
+print(Back.YELLOW + 'Sarı arka plan' + Back.RESET)
+print(Back.BLUE + 'Mavi arka plan' + Back.RESET)
+print(Back.MAGENTA + 'Magenta arka plan' + Back.RESET)
+print(Back.CYAN + 'Camgöbeği arka plan' + Back.RESET)
+print(Back.WHITE + 'Beyaz arka plan' + Back.RESET)
+
+print(Style.BRIGHT + 'Parlak metin' + Style.RESET_ALL)
+print(Style.NORMAL + 'Normal metin' + Style.RESET_ALL)
+print(Style.DIM + 'Sönük metin' + Style.RESET_ALL)
